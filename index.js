195b50ed3e89131e8d0e40623dff38e2645ea4a0
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2024-03-03 21:24:25 +0300

    variables

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.log("Website");
 
 document.getElementById("myH1").textContent= "Welcome to my website!";
+let age =  25;
+let firstName = "John";
+let lastName = "Doe";  
+let fullName = `${firstName} ${lastName}`; 
